d7438e2599cc6b94eac3687ba1fbb4048686c561
yusakwijayas <yusakwijayas@gmail.com>
2023-04-28 18:19:17 +0700

    initiate scss and some style

--- src/components/App.vue
+++ src/components/App.vue
@@ -4,46 +4,81 @@ import { RouterLink } from 'vue-router'
 
 <template id="app">
   <header>
-    <img alt="Vue logo" class="logo" src="/src/assets/logo.svg" width="125" height="125" />
+    <div class="brand">UX Glossary</div>
 
-    <div class="wrapper">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/user">User</router-link></li>
-        <li><router-link to="/project">Project</router-link></li>
-        <li><router-link to="/role">Role</router-link></li>
-        <li><router-link to="/search">Search</router-link></li>
-      </ul>
+    <div class="right">
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/user">User</router-link></li>
+          <li><router-link to="/project">Project</router-link></li>
+          <li><router-link to="/role">Role</router-link></li>
+          <li><router-link to="/search">Search</router-link></li>
+        </ul>
+      </nav>
+  
+      <div class="user">
+        <p>Hi, name here | <button><img src="../assets/icons/icon-logout.svg"></button></p>
+      </div>
     </div>
   </header>
   <main><router-view></router-view></main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
-  line-height: 1.5;
+  display: flex;
+  width: 100%;
+  max-width: 100%;
+  padding-inline: 1rem;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.brand {
+  flex: 1 1 auto;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.right {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  flex: 1 1 auto;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+nav ul {
+  display: flex;
+  list-style: none;
+  width: 100%;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  li {
+    padding-inline: 1rem;
+    font-weight: 500;
   }
 }
+// header {
+//   line-height: 1.5;
+// }
+
+// .logo {
+//   display: block;
+//   margin: 0 auto 2rem;
+// }
+
+// @media (min-width: 1024px) {
+//   header {
+//     display: flex;
+//     place-items: center;
+//     padding-right: calc(var(--section-gap) / 2);
+//   }
+
+//   .logo {
+//     margin: 0 2rem 0 0;
+//   }
+
+//   header .wrapper {
+//     display: flex;
+//     place-items: flex-start;
+//     flex-wrap: wrap;
+//   }
+// }
 </style>
