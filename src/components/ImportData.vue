848431ec95bc5dda51c30aa0a0aad37272d4bfb8
fnaufalh <fnaufalh@gmail.com>
2023-05-19 15:15:34 +0700

    import data success, and the function has disabled

--- src/components/ImportData.vue
+++ src/components/ImportData.vue
@@ -1,8 +1,10 @@
+<script type="module">
 /* This file will import JSON to Firebase if URL is executed. Comment this function if import
 process is done manually. */
-<script type="module">
 import { ImportDataRepository } from '@/repositories/ImportDataRepository'
-import atomic from '@/datas/atomic.json'
+/* Change this code below to import it */
+// import atomic from '@/datas/atomic.json'
+// import component from '@/datas/component.json'
 
 const importRepository = new ImportDataRepository()
 
@@ -10,21 +12,61 @@ export default {
   data() {
     return {
       components: [],
-      atomic: atomic
+      data: {
+        atomic: null,
+        component: [],
+        compare: []
+      }
     }
   },
   methods: {
-    addData(item) {
+    addAtomic(item) {
       importRepository.addAtomic(item)
+    },
+    addComponent(item) {
+      importRepository.addComponent(item)
+    },
+    async getAtomic() {
+      this.data.atomic = await importRepository.getAtomic()
     }
   },
   created() {
-    for (let i = 0; i < this.atomic.length; i++) {
-      this.addData(this.atomic[i].name)
-    }
+    /* Get Atomic before fetch the atomic and components */
+    // this.getAtomic()
+    /* Get the components */
+    // this.data.component = component
+    /* loop the atomic data to import */
+    // this.data.atomic = atomic
+    // for (let i = 0; i < this.data.atomic.length; i++) {
+    //   this.addAtomic(this.data.atomic[i].name)
+    // }
+    /* loop the component data to import */
+    // this.getAtomic().then(() => {
+    //   if (this.data.atomic) {
+    //     console.log('in')
+    //     for (let i = 0; i < this.data.component.length; i++) {
+    //       for (let j = 0; j < this.data.atomic.length; j++) {
+    //         if (this.data.component[i].atomic === this.data.atomic[j].name) {
+    //           /* compare to test the json and the atomic is well fetched */
+    //           // this.data.compare[i] =
+    //           //   this.data.component[i].atomic + ' X ' + this.data.atomic[j].name
+    //           /* Get the atomic ID and add the component */
+    //           this.data.component[i].atomic = this.data.atomic[j].id
+    //           this.addComponent(this.data.component[i])
+    //           console.log(this.data.component[i])
+    //         }
+    //       }
+    //     }
+    //   } else {
+    //     console.log('out')
+    //   }
+    // })
   }
 }
 </script>
 <template>
-  <div v-for="item in data" :key="item">{{ item.name }}</div>
+  <!-- <div v-for="item in this.data.atomic" :key="item">{{ item }}</div> -->
+  <!-- <div v-for="item in this.data.component" :key="item">{{ item }}</div> -->
+  <!-- <div v-for="item in this.data.compare" :key="item">{{ item }}</div> -->
+  <div></div>
 </template>
--- src/repositories/ImportDataRepository.ts
+++ src/repositories/ImportDataRepository.ts
@@ -2,12 +2,23 @@
   This file will import JSON to Firebase.
 */
 import { database } from './Firebase'
-import { addDoc, collection, doc, getDocs } from 'firebase/firestore'
+import { addDoc, collection, getDocs } from 'firebase/firestore'
 
-const data = collection(database, 'atomic')
+const atomic = collection(database, 'atomic')
+const component = collection(database, 'component')
 
 export class ImportDataRepository {
   public addAtomic(reference: string) {
-    addDoc(data, { name: reference })
+    addDoc(atomic, { name: reference })
+  }
+  public async getAtomic() {
+    const atomicSnapshot = await getDocs(atomic)
+    return atomicSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }))
+  }
+  public addComponent(reference: any) {
+    addDoc(component, reference)
   }
 }
